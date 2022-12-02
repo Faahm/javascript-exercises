895a9456fd325c773af979cfe5bf5cccebb51713
Faahm <fatimahussien2@gmail.com>
2022-12-02 20:24:50 +0800

    Finish Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const sum = arr.reduce(
+    (accumulator, currentNum) => accumulator + currentNum,
+    0
+  );
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  const sum = arr.reduce(
+    (accumulator, currentNum) => accumulator * currentNum,
+    1
+  );
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a < 0) return -1;
+  else if (a == 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
