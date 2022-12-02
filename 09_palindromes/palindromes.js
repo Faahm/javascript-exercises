a96afa3f3eca6edb4015c5d67344ad8ee056df70
Faahm <fatimahussien2@gmail.com>
2022-12-02 20:47:27 +0800

    Finish Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+  var re = /[\W]/g;
+  var lowRegS = s.toLowerCase().replace(re, "");
+  var reverseS = lowRegS.split("").reverse().join("");
+  return reverseS === lowRegS;
 };
 
 // Do not edit below this line
