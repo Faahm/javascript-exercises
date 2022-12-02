da923a4df589de9fe9c3bfe8a76e43af76f15176
Faahm <fatimahussien2@gmail.com>
2022-12-02 21:18:29 +0800

    Finish Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  } else {
+    let newNum = 0;
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 0; i < n; i++) {
+      newNum = num1 + num2;
+      num1 = num2;
+      num2 = newNum;
+    }
+    return num1;
+  }
 };
 
 // Do not edit below this line
